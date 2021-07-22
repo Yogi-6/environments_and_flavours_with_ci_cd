56e968b4a71564df2c034658ee8f0c48998b38b2
Yogesh <yogeshparwani99.yp@gmail.com>
2021-07-22 16:30:26 +0530

    Remove unused imports

--- lib/main/main_dev.dart
+++ lib/main/main_dev.dart
@@ -1,7 +1,5 @@
-import 'package:environments_and_flavours/config_reader.dart';
 import 'package:environments_and_flavours/environment.dart';
 import 'package:environments_and_flavours/main/main_common.dart';
-import 'package:flutter/material.dart';
 
 Future<void> main(List<String> args) async {
   await mainCommon(Environment.dev);
